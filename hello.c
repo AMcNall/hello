bd0d46081af877c3d97ba1a7d687fa2494d8f6e8
Andrew McNally <amcnally2@huskers.unl.edu>
2022-01-28 15:34:28 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Andrew McNally
+ *         amcnally@uno.edu
+ * Date: 2022/01/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Andrew McNally\n");
+
+  return 0;
+}
